fa792b95f26eac6373e9049e412a5516a02fe25b
Ben Jones <icompugeek@gmail.com>
2013-06-21 05:19:36 -0400

    1.1

--- loader.php
+++ loader.php
@@ -1,14 +1,14 @@
 <?php 
 /*
 Plugin Name: BP Auto Load Activity
-Plugin URI: http://wordpress.com
+Plugin URI: https://github.com/bphelp/bp-auto-load-activity
 Description: This plugin will auto load more in the activity stream
 Version: 1.1
 Requires at least: WordPress 3.2.1 / BuddyPress 1.5.1
 Tested up to: WordPress 3.5.1 / BuddyPress 1.7.1
 License: GNU/GPL 2
 Author: @bphelp
-Author URI: http://wordpress.com
+Author URI: http://bphelpblog.wordpress.com/
 */
 
 function bp_auto_load_activity_init() {
